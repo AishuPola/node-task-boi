27d22df40a91677c75a2e8cd039d1165a60a4de1
AishuPola <aishwarya.pola@proclink.com>
2024-10-07 21:43:56 +0530

    changed mail for sending mails to admin

--- services/email.service.js
+++ services/email.service.js
@@ -47,6 +47,7 @@ export async function sendingUserDetailsToAdmin(details) {
       host: "smtp.gmail.com",
       port: 587,
       secure: false,
+
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
